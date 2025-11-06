20f64f1879d825760918f98cb032c4c536fdb53c
Ennius5 <e.campomanes101848@gmail.com>
2025-11-06 18:43:37 +0800

    Revert "FIRST STEPS"
    This reverts commit 03ed0b9614d4deee82e68c86ebd1f5137c5af9d4.

--- backend/apps/Academics/models.py
+++ backend/apps/Academics/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-
-# On the academics models, the models shall appear for module 3:
-#Schedule_entry
-#Schedule_block
--- backend/apps/Organizations/models.py
+++ backend/apps/Organizations/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-#On the organizations shall these models appear for module 6:
-#Events
-#Event_attendance
-#Event_Schedule
-#event_Schedule_block
-#Event_Approvals
-#Event_type
