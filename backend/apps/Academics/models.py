9bfd278fdd5d5fcf812660e187b3754e8936e0e4
Ennius5 <e.campomanes101848@gmail.com>
2025-11-06 18:51:46 +0800

    Updated the branch.

--- backend/apps/Academics/models.py
+++ backend/apps/Academics/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from Users.models import * #Just import everything. the users model is greatly intertwined here
 # Create your models here.
+
+
+
+
+# On the academics models, the models shall appear for module 3:
+#Schedule_entry
+#Schedule_block
--- backend/apps/Organizations/models.py
+++ backend/apps/Organizations/models.py
@@ -1,3 +1,3 @@
 from django.db import models
-
+from Users.models import * #Just import everything. the users model is greatly intertwined here
 # Create your models here.
